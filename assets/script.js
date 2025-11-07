82168f7e7c2bae1c64cc01b234e317a805dd5449
cogito-blog <zoeyuyzy@gmail.com>
2025-11-07 16:06:13 +0800

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// 填充年份
+document.getElementById('year')?.textContent = new Date().getFullYear();
+document.getElementById('year-2')?.textContent = new Date().getFullYear();
+document.getElementById('year-3')?.textContent = new Date().getFullYear();
+document.getElementById('year-4')?.textContent = new Date().getFullYear();
+document.getElementById('year-5')?.textContent = new Date().getFullYear();
